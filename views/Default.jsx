99d29fe6c093a47234fbdc0e49cbd00fc1cb4202
Julio Colon <jcolon1092@gmail.com>
2022-03-09 20:43:43 -0500

    added footer to default view

--- views/Default.jsx
+++ views/Default.jsx
@@ -43,16 +43,39 @@ class DefaultLayout extends React.Component {
                     <main>
                         {this.props.children}
                     </main>
-                    <footer>
+                    <footer class="page-footer pt-4">
+
+
+                        <div class="container-sm text-center text-md-left">
+
+                            <div class="row">
+
+
+                                <div class="col text-center">
+
+                                    <h5 class="text-uppercase">SNEAKERS</h5>
+                                    <p>Contact: JCOLON1092@GMAIL.COM</p>
+                                    <p>Twitter: @JCCOLOGNE</p>
+                                    <p>Instagram:@_JULIOCOLON</p>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="footer-copyright text-center py-3">© 2022 Copyright:
+                            <a href="#"> Julio Colon</a>
+                        </div>
 
                     </footer>
                     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
                     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
 
-                </body>
+                </body >
 
-            </html>
+            </html >
 
         )
     }
