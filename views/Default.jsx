480b235ad7c77a262af437e2380f8fc2f63b6f2a
Julio Colon <jcolon1092@gmail.com>
2022-03-14 21:13:03 -0400

    edit to Default view

--- views/Default.jsx
+++ views/Default.jsx
@@ -13,7 +13,7 @@ class DefaultLayout extends React.Component {
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" />
-                    <title>SNEAKERS</title>
+                    <title>Sneakers</title>
                     <link rel="icon" href="/images/favicon-32x32.png" sizes="32x32" />
                     <link rel="stylesheet" href="/css/app.css" />
                 </head>
